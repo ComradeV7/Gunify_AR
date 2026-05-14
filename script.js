d1d894d124a46578a65411a118dc0cfc7cbe90d8
Krish Bharadwaj <krishbharadwaj43@gmail.com>
2026-05-14 18:17:52 +0530

    Major Changes

--- script.js
+++ script.js
@@ -1,62 +1,4 @@
-const gunDatabase = {
-  krsvMarker: {
-    title: "KRISS Vector",
-    description:
-      "A modern submachine gun known for its low recoil and high rate of fire.",
-    specs: ["Caliber: 9mm", "Range: 120m", "Fire Mode: Semi-Auto / Burst"],
-    fire: "./sounds/KRSV.mp3",
-    reload: "./reload/KRSV.mp3",
-  },
-
-  ak47Marker: {
-    title: "AK-47",
-    description:
-      "Legendary Soviet assault rifle known for its durability and reliability in harsh conditions.",
-    specs: [
-      "Caliber: 7.62×39mm",
-      "Range: 350m",
-      "Fire Mode: Semi-Auto / Full-Auto",
-    ],
-    fire: "./sounds/AK47.mp3",
-    reload: "./reload/AK47.mp3",
-  },
-  smithMarker: {
-    title: "Smith & Wesson M625",
-    description:
-      "Modern semi-automatic pistol used by law enforcement and civilians.",
-    specs: ["Caliber: 9×19mm", "Range: 50m", "Fire Mode: Semi-Auto"],
-    fire: "./sounds/Smith.mp3",
-    reload: "./reload/Smith.mp3",
-  },
-  mosinMarker: {
-    title: "Mosin Nagant",
-    description:
-      "A five-shot military rifle used by the armed forces of the Russian Empire.",
-    specs: ["Caliber: 7.62×54mm", "Range: 500m", "Fire Mode: Bolt-Action"],
-    fire: "./sounds/Mosin.mp3",
-    reload: "./reload/Mosin.mp3",
-  },
-  uziMarker: {
-    title: "Uzi",
-    description:
-      "A compact Israeli open-bolt submachine gun, widely used by military and security forces worldwide.",
-    specs: [
-      "Caliber: 9×19mm",
-      "Range: 200m",
-      "Fire Mode: Full-Auto / Semi-Auto",
-    ],
-    fire: "./sounds/Uzi.mp3",
-    reload: "./reload/Uzi.mp3",
-  },
-  sigsauerMarker: {
-    title: "Sig Sauer",
-    description:
-      "A highly reliable pistol, known for its durability. Its smooth trigger pull make it a favorite for civilian shooters.",
-    specs: ["Caliber: 9×19mm", "Range: 50m", "Fire Mode: Semi-Auto"],
-    fire: "./sounds/SigSauer.mp3",
-    reload: "./reload/SigSauer.mp3",
-  },
-};
+import { weaponsDatabase } from "./weaponsData.js";
 
 let fireAudio = new Audio(); // fire auio
 let reloadAudio = new Audio(); // reload audio
@@ -92,7 +34,7 @@ closeButton.addEventListener("click", () => {
 
 // Function to update the info panel
 function updateInfoPanel(gunId) {
-  const gunData = gunDatabase[gunId];
+  const gunData = weaponsDatabase[gunId];
 
   if (gunData) {
     gunTitle.textContent = gunData.title;
@@ -123,7 +65,7 @@ function hideInfo() {
   infoPanel.style.display = "none";
 }
 
-Object.keys(gunDatabase).forEach((gunId) => {
+Object.keys(weaponsDatabase).forEach((gunId) => {
   let marker = document.getElementById(gunId);
 
   marker.addEventListener("markerFound", () => {
